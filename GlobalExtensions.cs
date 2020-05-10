12228d54dc54be71e2e3bfe6e151001c1b1e63d6
Callum Carmicheal <callumcarmicheal@gmail.com>
2020-05-10 01:44:14 +0100

    Created GlobalExtensions and ScaleDown helper method
    Created a extenson method for CornerRadius to scale it down, this is for usage inside the NodeView file

---
+++ GlobalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NodeGraph {
+    public static class GlobalExtensions {
+
+        public static CornerRadius ScaleDown(this CornerRadius x, double scale) {
+            if (scale == 0)
+                return new CornerRadius(0);
+
+            return new CornerRadius(
+                x.TopLeft / scale, x.TopRight / scale, x.BottomRight / scale, x.BottomLeft / scale);
+        }
+
+    }
+}
